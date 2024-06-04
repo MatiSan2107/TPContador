ae412ffce2f17d7ed9e0c2c759a4e9c497db8aa4
Matias <matiassandovalkm5@gmail.com>
2024-06-03 21:58:37 -0300

    esquema

---
+++ src/app/index.js
@@ -0,0 +1,27 @@
+// "use client"; // Esto hace que el componente se trate como un componente del cliente
+
+// import React, { useState } from "react";
+
+// export default function Home() {
+//   const [count, setCount] = useState(0);
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+//       <main className="text-center">
+//         <h1 className="text-3xl mb-4">Trabajo Practico 4</h1>
+//         <h2 className="text-2xl mb-4">Contador: {count}</h2>
+//         <button 
+//           className="bg-blue-500 text-white px-4 py-2 rounded"
+//           onClick={increment}
+//         >
+//           Sumar
+//         </button>
+//       </main>
+//     </div>
+//   );
+// }
+
